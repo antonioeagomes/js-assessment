f7c5df5b4bd366cb3085f294165b6da8e1a17c50
Antonio Emmanuel Araujo Gomes <emmanuelgomes@pmenos.com.br>
2020-09-22 14:31:22 -0300

    Async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+	async: function (value) {
+		const promise = new Promise((resolve) => resolve(value));
+		return promise;
+	},
 
-  },
-
-  manipulateRemoteData: function(url) {
-
-  }
+	manipulateRemoteData: function (url) {
+		return new Promise((resolve, reject) => {
+			const xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.onload = () => {
+				if (xhr.status === 200) resolve(xhr.response);
+				else reject(Error(xhr.statusText));
+			};
+			xhr.send();
+		}).then((response) => {
+			const data = JSON.parse(response).people;
+			return data.map((k) => k.name).sort();
+		});
+	},
 };
