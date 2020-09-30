71330c29c4f8d619c6e20e461ed735f92138f63e
Emmanuel Gomes <aeag@outlook.com>
2020-09-30 17:22:23 -0300

    strings

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,34 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
+	reduceString: function (str, amount) {},
 
-  },
+	wordWrap: function (str, cols) {
+		const words = str.split(' ');
+		var result = '';
+		let currentLength = 0;
+		let word = '';
+		let newLine = '';
 
-  wordWrap: function(str, cols) {
+		for (let i = 0; i < words.length; i++) {
+			word = words[i];
+			currentLength += word.length;
+			if (i === 0) {
+				newLine = '';
+			} else if (currentLength > cols) {
+				currentLength = word.length;
+				newLine = '\n';
+			} else {
+				newLine = ' ';
+			}
 
-  },
+			result += newLine + word;
+		}
 
-  reverseString: function(str) {
+		return result;
+	},
 
-  }
+	reverseString: function (str) {
+		return str.split('').reverse().join('');
+	},
 };
